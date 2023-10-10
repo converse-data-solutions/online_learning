6c01221c4f08f158158abe5837e341f55fc4e07a
Aravindh456 <aravindh@conversedatasolutions.com>
2023-10-10 22:45:16 +0530

    Validation for admin users status update (active, inactive)

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -3,7 +3,7 @@ class Admin::UsersController < ApplicationController
     before_action :check_admin_role
     
     def index
-        @users = User.active.all
+        @users = User.all
         @new_admin = User.new
       end
       
@@ -29,16 +29,56 @@ class Admin::UsersController < ApplicationController
         @user = User.find(params[:id])
       end
     
+      # def update
+      #   @user = User.find(params[:id])
+      #   if @user.add_role(user_params[:role])
+      #     flash[:notice] = "User information updated successfully."
+      #     redirect_to admin_users_path
+      #   else
+      #     flash[:alert] = "Failed to update user information."
+      #     render :edit
+      #   end
+      # end
+
+
+      # def update
+      #   @user = User.find(params[:id])
+      
+      #   if @user == current_user
+      #     flash[:alert] = "You cannot change your own status."
+      #   else
+      #     new_status = params[:user][:deleted].to_s == "true" # Convert to boolean
+      #     if @user.update(deleted: new_status)
+      #       flash[:notice] = @user.deleted? ? "User is now inactive." : "User is now active."
+      #     else
+      #       flash[:alert] = "Failed to update user status."
+      #     end
+      #   end
+      
+      #   redirect_to admin_users_path
+      # end
+
+
+
       def update
         @user = User.find(params[:id])
-        if @user.add_role(user_params[:role])
-          flash[:notice] = "User information updated successfully."
-          redirect_to admin_users_path
+      
+        if @user == current_user
+          flash[:alert] = "You cannot change your own status."
         else
-          flash[:alert] = "Failed to update user information."
-          render :edit
+          new_role = user_params[:role] # Get the new role from the form
+          new_status = params[:user][:deleted].to_s == "true" # Convert to boolean
+      
+          if @user.add_role(new_role) && @user.update(deleted: new_status)
+            flash[:notice] = "User information updated successfully."
+          else
+            flash[:alert] = "Failed to update user information."
+          end
         end
+      
+        redirect_to admin_users_path
       end
+      
   
     def destroy
       user = User.find(params[:id])
