b46369eb4acd91d15d0a868eddb7abcbc6150bfe
Aravindh456 <aravindh@conversedatasolutions.com>
2023-10-20 23:23:25 +0530

    video play next previous

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class Users::PasswordsController < Devise::PasswordsController
-  before_update :after_resetting_password_path_for, only: %i[edit update]
   # GET /resource/password/new
   # def new
   #   super
@@ -13,26 +12,20 @@ class Users::PasswordsController < Devise::PasswordsController
   # end
 
   # GET /resource/password/edit?reset_password_token=abcdef
-  def edit
-    super
-    sign_out(resource)
-    redirect_to new_user_session_path
-  end
+  # def edit
+  #   super
+  # end
 
   # PUT /resource/password
-  def update
-    super
-    sign_out(resource)
-    redirect_to new_user_session_path
-  end
+  # def update
+  #   super
+  # end
 
-  protected
+  # protected
 
-  def after_resetting_password_path_for(resource)
-    super(resource)
-    sign_out(resource)
-    new_user_session_path
-  end
+  # def after_resetting_password_path_for(resource)
+  #   super(resource)
+  # end
 
   # The path used after sending reset password instructions
   # def after_sending_reset_password_instructions_path_for(resource_name)
