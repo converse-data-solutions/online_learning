b481efb0941156124b70a27f89c918f1ed9a9088
aravindh-converse <aravindh@conversedatasolutions.com>
2023-10-26 16:50:22 +0530

    Fixes

--- config/initializers/app_config.rb
+++ config/initializers/app_config.rb
@@ -1,5 +1,5 @@
 APP_CONFIG = YAML.load(
-  File.read(File.expand_path('/home/aravindh/projects/online_learning/config/app_config.yml',
+  File.read(File.expand_path('../app_config.yml',
                              __dir__)), symbolize_names: true, aliases: true
 )
 
