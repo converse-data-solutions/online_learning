219bb8e61d86a41103b876dce067f8e620fdf3c5
aravindh-converse <aravindh@conversedatasolutions.com>
2024-01-06 20:41:53 +0530

    added notify js for both positive and negative

--- app/controllers/admin/students_controller.rb
+++ app/controllers/admin/students_controller.rb
@@ -19,10 +19,16 @@ class Admin::StudentsController < ApplicationController
     @student = User.new(student_params)
     respond_to do |format|
       if @student.add_role_and_save(student_params[:role])
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('user-admin-form', partial: 'admin/students/show', locals: { student: @student }) }
+        get_students
+        format.turbo_stream
         format.json { render :show, status: :created, location: admin_student_url(@student) }
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.replace('user-admin-form', partial: 'admin/students/form', locals: { student: @student }) }
+        format.turbo_stream do
+          render turbo_stream: [
+              turbo_stream.replace('user-admin-form', partial: 'admin/students/form', locals: { student: @student }),
+              turbo_stream.append('user-table', partial: 'shared/flash', locals: { message: 'Student creation failed.', type: 'notice' })
+          ]
+        end
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
@@ -36,25 +42,37 @@ class Admin::StudentsController < ApplicationController
     redirect_to admin_students_path
   end
 
-  def update
+  def update # rubocop:disable Metrics/AbcSize,Metrics/MethodLength
     respond_to do |format|
       if @student.update(student_params)
-        format.turbo_stream { redirect_to admin_students_path, notice: 'Student updated successfully' }
+        get_students
+        format.turbo_stream
         format.json { render :show, status: :ok, location: admin_student_url(@student) }
       else
-        format.turbo_stream { render turbo_stream: turbo_stream.update('edit-student-popup', partial: 'admin/students/edit', locals: { student: @student }) }
+        format.turbo_stream do
+          render turbo_stream: [
+            turbo_stream.update('edit-student-popup', partial: 'admin/students/edit', locals: { student: @student }),
+            turbo_stream.append('user-table', partial: 'shared/flash', locals: { message: 'Student update failed.', type: 'notice' })
+          ]
+        end
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  def destroy
+  def destroy # rubocop:disable Metrics/MethodLength,Metrics/AbcSize
     respond_to do |format|
       if @student.update(deleted: true)
-        format.turbo_stream { redirect_to admin_students_path, notice: 'User deleted successfully' }
+        get_students
+        format.turbo_stream do
+          render turbo_stream: [
+           turbo_stream.append('user-table', partial: 'shared/flash', locals: { message: 'Student was successfully destroyed.', type: 'notice' }),  # rubocop:disable Layout/FirstArrayElementIndentation
+           turbo_stream.update('user-table', partial: 'admin/students/table', locals: { students: @students })
+          ]
+        end
         format.json { render :show, status: :ok, location: admin_student_url(@student) }
       else
-        format.turbo_stream { redirect_to admin_students_path, notice: 'User destroy failed' }
+        format.turbo_stream { render turbo_stream: turbo_stream.append('user-table', partial: 'shared/flash', locals: { message: 'Student destroy failed.', type: 'notice' }) }
         format.json { render json: @student.errors, status: :unprocessable_entity }
       end
     end
@@ -63,7 +81,7 @@ class Admin::StudentsController < ApplicationController
   private
 
   def get_students
-    @students = User.student.includes(user_courses: [:course]).search_by_name_and_email(params[:search]).paginate(page: params[:page] || 1, per_page: params[:per_page] || 5)
+    @students = User.student.order(name: :asc).includes(user_courses: [:course]).search_by_name_and_email(params[:search]).paginate(page: params[:page] || 1, per_page: params[:per_page] || 5)
   end
 
   def set_student
