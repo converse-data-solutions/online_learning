9b2c0b54a628835bf1a1b4e421f09437e3c3ef55
aravindh-converse <aravindh@conversedatasolutions.com>
2024-02-08 21:58:49 +0530

    new section controller changed to under admin

--- test/controllers/admin/course_sections_controller_test.rb
+++ test/controllers/admin/course_sections_controller_test.rb
@@ -1,6 +1,6 @@
 require "test_helper"
 
-class CourseSectionsControllerTest < ActionDispatch::IntegrationTest
+class Admin::CourseSectionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
